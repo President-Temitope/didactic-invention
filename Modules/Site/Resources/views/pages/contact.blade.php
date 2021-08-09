2a2c00e2d1e7033d7aeed1534201c257ec4550ca
callmehalpha <abdulazeezyisau@gmail.com>
2021-08-09 22:24:42 +0100

    other pages fix

--- Modules/Site/Resources/views/pages/contact.blade.php
+++ Modules/Site/Resources/views/pages/contact.blade.php
@@ -1,104 +1,153 @@
 @extends('site::layouts.master')
 @section('content')
-    <!-- Banners Start -->
-    <div class="banner contact" id="home" style="background-image: url('assets/img/bg-2.html')">
-        <!-- hero-area Start -->
-        <div class="hero-area">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-6 col-lg-8 wow fadeInUp" data-wow-duration="0.3s" data-wow-delay="0.3s">
-                        <div class="banner-content">
-                            <h2 class="title">Contact</h2>
+    <!-- Banner Area Starts -->
+    <section class="banner-area">
+        <div class="banner-overlay">
+            <div class="banner-text text-center">
+                <div class="container">
+                    <!-- Section Title Starts -->
+                    <div class="row text-center">
+                        <div class="col-xs-12">
+                            <!-- Title Starts -->
+                            <h2 class="title-head">Get in <span>touch</span></h2>
+                            <!-- Title Ends -->
+                            <hr>
+                            <!-- Breadcrumb Starts -->
                             <ul class="breadcrumb">
-                                <li>
-                                    <a href="/">Home</a> <i class="flaticon-airplane49"></i>
-                                </li>
-                                <li>
-                                    <a href="#">Pages</a> <i class="flaticon-airplane49"></i>
-                                </li>
-                                <li>
-                                    Contact
-                                </li>
+                                <li><a href="/"> home</a></li>
+                                <li>contact</li>
                             </ul>
+                            <!-- Breadcrumb Ends -->
                         </div>
                     </div>
+                    <!-- Section Title Ends -->
                 </div>
             </div>
         </div>
-    </div>
-
-    <!-- Contact-section Start -->
-    <section class="contact-section">
+    </section>
+    <!-- Banner Area Ends -->
+    <!-- Contact Section Starts -->
+    <section class="contact">
         <div class="container">
-            <div class="row justify-content-center justify-content-lg-between">
-                <div class="col-lg-7">
-                    <div class="contact-wrapper wow fadeInUp" data-wow-duration="0.3s" data-wow-delay="0.3s">
-                        <h3 class="subtitle">Get in Touch</h3>
-                        <form class="contact-form" id="contact_form_submit">
-                            <div class="form-group">
-                                <label for="name">Your Name</label>
-                                <input type="text" placeholder="Enter Your Full Name" id="name" required="">
-                            </div>
-                            <div class="form-group">
-                                <label for="email">Your Email </label>
-                                <input type="text" placeholder="Enter Your Email " id="email" required="">
-                            </div>
-                            <div class="form-group m-0">
-                                <label for="message">Your Message </label>
-                                <textarea id="message" placeholder="Enter Your Message" required=""></textarea>
-                                <div class="form-check">
-                                    <input type="checkbox" id="check" checked="">
-                                    <label for="check">I agree to receive emails, newsletters and promotional
-                                        messages</label>
+            <div class="row">
+                <div class="col-xs-12 col-md-8 contact-form">
+                    <h3 class="col-xs-12">feel free to drop us a message</h3>
+                    <p class="col-xs-12">Need to speak to us? Do you have any queries or suggestions? Please contact us about all enquiries including membership and volunteer work using the form below.</p>
+                    <!-- Contact Form Starts -->
+                    <form class="form-contact" method="post" action="">
+                        @csrf
+                        <!-- Input Field Starts -->
+                        <div class="form-group col-md-6">
+                            <input class="form-control" name="firstname" id="firstname" placeholder="FIRST NAME" type="text" required>
+                        </div>
+                        <!-- Input Field Ends -->
+                        <!-- Input Field Starts -->
+                        <div class="form-group col-md-6">
+                            <input class="form-control" name="lastname" id="lastname" placeholder="LAST NAME" type="text" required>
+                        </div>
+                        <!-- Input Field Ends -->
+                        <!-- Input Field Starts -->
+                        <div class="form-group col-md-6">
+                            <input class="form-control" name="email" id="email" placeholder="EMAIL" type="email" required>
+                        </div>
+                        <!-- Input Field Ends -->
+                        <!-- Input Field Starts -->
+                        <div class="form-group col-md-6">
+                            <input class="form-control" name="text" id="subject" placeholder="SUBJECT" type="text" required>
+                        </div>
+                        <!-- Input Field Ends -->
+                        <!-- Input Field Starts -->
+                        <div class="form-group col-xs-12">
+                            <textarea class="form-control" id="message" name="message" placeholder="MESSAGE" required></textarea>
+                        </div>
+                        <!-- Input Field Ends -->
+                        <!-- Submit Form Button Starts -->
+                        <div class="form-group col-xs-12 col-sm-4">
+                            <button class="btn btn-primary btn-contact" type="submit">send message</button>
+                        </div>
+                        <!-- Submit Form Button Ends -->
+                        <!-- Form Submit Message Starts -->
+                        <div class="col-xs-12 text-center output_message_holder d-none">
+                            <p class="output_message"></p>
+                        </div>
+                        <!-- Form Submit Message Ends -->
+                    </form>
+                    <!-- Contact Form Ends -->
+                </div>
+                <!-- Contact Widget Starts -->
+                <div class="col-xs-12 col-md-4">
+                    <div class="widget">
+                        <div class="contact-page-info">
+                            <!-- Contact Info Box Starts -->
+                            <div class="contact-info-box">
+                                <i class="fa fa-home big-icon"></i>
+                                <div class="contact-info-box-content">
+                                    <h4>Address</h4>
+                                    <p>123 Disney Street Slim Av. Brooklyn Bridge, NY, New York</p>
                                 </div>
                             </div>
-                            <div class="form-group text-center m-0">
-                                <input class="button-1" type="submit" value="Send Message">
+                            <!-- Contact Info Box Ends -->
+                            <!-- Contact Info Box Starts -->
+                            <div class="contact-info-box">
+                                <i class="fa fa-phone big-icon"></i>
+                                <div class="contact-info-box-content">
+                                    <h4>Phone Numbers</h4>
+                                    <p>+88 0123 4567 890<br>+88 0231 3421 453</p>
+                                </div>
                             </div>
-                        </form>
+                            <!-- Contact Info Box Ends -->
+                            <!-- Contact Info Box Starts -->
+                            <div class="contact-info-box">
+                                <i class="fa fa-envelope big-icon"></i>
+                                <div class="contact-info-box-content">
+                                    <h4>Email Addresses</h4>
+
+                                    <p>contact@example.com<br>info@example.com</p>
+                                </div>
+                            </div>
+                            <!-- Contact Info Box Ends -->
+                            <!-- Social Media Icons Starts -->
+                            <div class="contact-info-box">
+                                <i class="fa fa-share-alt big-icon"></i>
+                                <div class="contact-info-box-content">
+                                    <h4>Social Profiles</h4>
+                                    <div class="social-contact">
+                                        <ul>
+                                            <li class="facebook"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                                            <li class="twitter"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                                            <li class="google-plus"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Social Media Icons Starts -->
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="contact-content">
-                        <div class="section-header wow fadeInUp" data-wow-duration="0.3s" data-wow-delay="0.3s">
-                            <h3 class="subtitle">Have questions?</h3>
-                            <p class="text">Have questions about payments or price
-                                plans? We have answers!</p>
-                            <a href="{{route('faq')}}">Read F.A.Q <i class="fas fa-angle-right"></i></a>
+                <!-- Contact Widget Ends -->
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section Ends -->
+    <!-- Call To Action Section Starts -->
+    <section class="call-action-all">
+        <div class="call-action-all-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <!-- Call To Action Text Starts -->
+                        <div class="action-text">
+                            <h2>Get Started Today With Bitcoin</h2>
+                            <p class="lead">Open account for free and start trading Bitcoins!</p>
                         </div>
-                        <div class="contact-area">
-                            <div class="contact-item wow fadeInUp" data-wow-duration="0.3s" data-wow-delay="0.3s">
-                                <div class="contact-thumb">
-                                    <img src="assets/img/contact1.png" alt="contact">
-                                </div>
-                                <div class="contact-contact">
-                                    <h4 class="lasthead">Email Us</h4>
-                                    <a href="Mailto:info@mosto.com">info@mosto.com</a>
-                                </div>
-                            </div>
-                            <div class="contact-item wow fadeInUp" data-wow-duration="0.3s" data-wow-delay="0.3s">
-                                <div class="contact-thumb">
-                                    <img src="assets/img/contact2.png" alt="contact">
-                                </div>
-                                <div class="contact-contact">
-                                    <h4 class="lasthead">Call Us</h4>
-                                    <a href="Tel:565656855">+1 (987) 664-32-11</a>
-                                    <a href="Tel:565656855">+1 (987) 664-32-11</a>
-                                </div>
-                            </div>
-                            <div class="contact-item wow fadeInUp" data-wow-duration="0.3s" data-wow-delay="0.3s">
-                                <div class="contact-thumb">
-                                    <img src="assets/img/contact3.png" alt="contact">
-                                </div>
-                                <div class="contact-contact">
-                                    <h4 class="lasthead">Visit Us</h4>
-                                    <p>4293 Euclid Avenue, Los Angeles,CA 90012</p>
-                                </div>
-                            </div>
-                        </div>
+                        <!-- Call To Action Text Ends -->
+                        <!-- Call To Action Button Starts -->
+                        <p class="action-btn"><a class="btn btn-primary" href="{{route('register')}}">Register Now</a></p>
+                        <!-- Call To Action Button Ends -->
                     </div>
                 </div>
             </div>
         </div>
     </section>
+    <!-- Call To Action Section Ends -->
 @endsection
